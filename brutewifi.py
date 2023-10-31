64886adfa8a313b1dc1077b2fd701fbaafa2c589
vyntriax <149506913+vyntriax@users.noreply.github.com>
2023-10-31 15:54:37 +0100

    Update brutewifi.py

--- brutewifi.py
+++ brutewifi.py
@@ -1,13 +1,20 @@
-
-import subprocess
+import pyautogui
+import time
 
 def open_wifi_settings():
     try:
         # Open Android settings and navigate to Wi-Fi settings
-        subprocess.run(['am', 'start', '-a', 'android.settings.SETTINGS'])
-        subprocess.run(['input', 'keyevent', '20'])
-        subprocess.run(['input', 'keyevent', '20'])
-        subprocess.run(['input', 'keyevent', '66'])
+        pyautogui.hotkey('ctrl', 'esc')  # Open recent apps (assuming it opens with the home button)
+        time.sleep(1)  # Wait for recent apps to open
+        pyautogui.press('esc')  # Close recent apps (if necessary)
+        pyautogui.press('win')  # Open app drawer (assuming it opens with the home button)
+        time.sleep(1)  # Wait for app drawer to open
+        pyautogui.typewrite('Settings')  # Type 'Settings' to search for the Settings app
+        time.sleep(1)  # Wait for search results to appear
+        pyautogui.press('enter')  # Open the Settings app
+        time.sleep(1)  # Wait for the Settings app to open
+        pyautogui.press('tab', presses=4)  # Navigate to the Wi-Fi tab (adjust the number of tab presses as needed)
+        pyautogui.press('enter')  # Enter Wi-Fi settings
         print('Wi-Fi settings opened successfully.')
     except Exception as e:
         print('Error:', e)
