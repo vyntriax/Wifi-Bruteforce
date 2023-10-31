d13f2ce738d8487ff6fa898ac867e5d926df3c28
vyntriax <149506913+vyntriax@users.noreply.github.com>
2023-10-31 15:59:22 +0100

    Update brutewifi.py

--- brutewifi.py
+++ brutewifi.py
@@ -1,23 +1,68 @@
-import pyautogui
-import time
+import os
+
+# ANSI color codes for text formatting
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    END = '\033[0m'
+
+# Function to display the logo with color
+def display_logo():
+    print(f"{Colors.RED}Your Logo Here{Colors.END}")
+    print(f"{Colors.GREEN}Welcome to Wi-Fi Passwords Tool{Colors.END}")
+
+# Function to display menu with colors
+def display_menu():
+    print("\nMenu:")
+    print(f"{Colors.BLUE}1. Option 1{Colors.END}")
+    print(f"{Colors.BLUE}2. Option 2{Colors.END}")
+    print(f"{Colors.BLUE}3. Option 3{Colors.END}")
+    print(f"{Colors.YELLOW}4. Exit{Colors.END}")
+    choice = input(f"{Colors.GREEN}Enter your choice: {Colors.END}")
+    return choice
+
+# Main function
+def main():
+    os.system('clear')  # Clear the terminal screen
+    display_logo()  # Display the logo
+    while True:
+        choice = display_menu()  # Display the menu and get user choice
+        if choice == '1':
+            import subprocess
 
 def open_wifi_settings():
     try:
         # Open Android settings and navigate to Wi-Fi settings
-        pyautogui.hotkey('ctrl', 'esc')  # Open recent apps (assuming it opens with the home button)
-        time.sleep(1)  # Wait for recent apps to open
-        pyautogui.press('esc')  # Close recent apps (if necessary)
-        pyautogui.press('win')  # Open app drawer (assuming it opens with the home button)
-        time.sleep(1)  # Wait for app drawer to open
-        pyautogui.typewrite('Settings')  # Type 'Settings' to search for the Settings app
-        time.sleep(1)  # Wait for search results to appear
-        pyautogui.press('enter')  # Open the Settings app
-        time.sleep(1)  # Wait for the Settings app to open
-        pyautogui.press('tab', presses=4)  # Navigate to the Wi-Fi tab (adjust the number of tab presses as needed)
-        pyautogui.press('enter')  # Enter Wi-Fi settings
+        subprocess.run(['am', 'start', '-a', 'android.settings.SETTINGS'])
+        subprocess.run(['input', 'keyevent', '20'])
+        subprocess.run(['input', 'keyevent', '20'])
+        subprocess.run(['input', 'keyevent', '66'])
         print('Wi-Fi settings opened successfully.')
     except Exception as e:
         print('Error:', e)
+
+# Call the function to open Wi-Fi settings
+open_wifi_settings()
+            print("You chose Option 1")
+        elif choice == '2':
+            # Implement option 2 functionality
+            print("You chose Option 2")
+        elif choice == '3':
+            # Implement option 3 functionality
+            print("You chose Option 3")
+        elif choice == '4':
+            # Exit the program
+            print(f"{Colors.YELLOW}Exiting...{Colors.END}")
+            break
+        else:
+            # Invalid choice
+            print(f"{Colors.RED}Invalid choice. Please try again.{Colors.END}")
+
+# Run the main function if the script is run directly
+if __name__ == "__main__":
+    main()
 
 # Call the function to open Wi-Fi settings
 open_wifi_settings()
